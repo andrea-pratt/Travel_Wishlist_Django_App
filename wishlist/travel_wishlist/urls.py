cb83817941d5b8408754f81df6974888c2a53b37
Andrea <apratt1013@gmail.com>
2021-03-24 11:12:11 -0500

    add links between pages

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.place_list, name='place_list')
+    path('', views.place_list, name='place_list'),
+    path('visited', views.places_visited, name='places_visited')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Place
+from .forms import NewPlaceForm
 
 
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', { 'places' : places, 'new_place_form': new_place_form })
+
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited' : visited})
